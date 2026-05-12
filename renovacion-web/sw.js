9b592a058f2e1244f17734bcdedfeda8caa32d41
Vicente Meza <vmeza3501@gmail.com>
2026-05-12 13:41:39 -0400

    fix responsive settings

--- renovacion-web/sw.js
+++ renovacion-web/sw.js
@@ -1,5 +1,5 @@
 // IMPORTANTE: Cambia el número de esta versión (v3, v4, v5...) cada vez que subas cambios a GitHub
-const CACHE_NAME = 'renovacion-v10'; 
+const CACHE_NAME = 'renovacion-v11'; 
 
 const urlsToCache = [
     './',
